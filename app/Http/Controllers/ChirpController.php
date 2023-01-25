bdf36601a621cac577703f534242d5fc4f7a555a
root <root@guManzano_dev>
2023-01-25 20:07:12 -0300

    chore: form request for chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreChirpRequest;
 use App\Models\Chirp;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -15,17 +17,15 @@ class ChirpController extends Controller
     public function index(): Response
     {
         return Inertia::render('Chirps/Index', [
-            'chirps' => Chirp::with('user:id, name')
+            'chirps' => Chirp::with('user:id,name')
                 ->latest()
                 ->get()
         ]);
     }
 
-    public function store(Request $request): Redirector|Application|RedirectResponse
+    public function store(StoreChirpRequest $request): Redirector|Application|RedirectResponse
     {
-        $validated = $request->validate([
-            'message' => 'required|string|max:255',
-        ]);
+        $validated = $request->validated();
 
         $request->user()->chirps()->create($validated);
 
@@ -47,9 +47,13 @@ class ChirpController extends Controller
         //
     }
 
-    public function update(Request $request, Chirp $chirp)
+    public function update(StoreChirpRequest $request, Chirp $chirp): Redirector|Application|RedirectResponse
     {
-        //
+        $validated = $request->validated();
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     public function destroy(Chirp $chirp)
